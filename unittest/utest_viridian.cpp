e586f1139e94f6287fac1ba050cd0b3d78c245fd
mitchbur <mitchbur@nwlink.com>
2018-09-01 10:28:33 -0700

    Corrected version header generation. Added unit test for library version.

--- unittest/utest_viridian.cpp
+++ unittest/utest_viridian.cpp
@@ -1,1 +1,11 @@
 #include "viridian.h"
+#include <gtest/gtest.h>
+
+TEST( viridian, version )
+{
+	int version_major = VIRIDIAN_VERSION_MAJOR;
+	int version_minor = VIRIDIAN_VERSION_MINOR;
+	
+	EXPECT_EQ( version_major, 0 );
+	EXPECT_GT( version_minor, 0 );
+}
